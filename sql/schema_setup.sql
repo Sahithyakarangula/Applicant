de912553558718c95643a208b855d1014935a525
Sahithyakarangula <157728165+Sahithyakarangula@users.noreply.github.com>
2024-04-02 19:35:20 +0100

    Update schema_setup.sql

--- sql/schema_setup.sql
+++ sql/schema_setup.sql
@@ -1,1 +1,17 @@
 -- SQL queries to set up schemas in BigQuery
+CREATE TABLE your_dataset.js_posts (
+    id INT64,
+    title STRING,
+    body STRING,
+    user_id INT64,
+    status STRING,
+    user_name STRING,
+    user_email STRING
+);
+CREATE TABLE your_dataset.js_users (
+    id INT64,
+    name STRING,
+    username STRING,
+    email STRING
+);
+-- SQL queries to set up schemas in BigQuery
